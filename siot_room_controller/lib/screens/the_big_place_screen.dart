49032ba2f7188cf52bb283235a61b206d9c74b3d
Pytode <49377897+ongjiachun@users.noreply.github.com>
2021-01-10 20:08:36 +0800

    Done with The Big Place's settings. Optimized the code.

--- siot_room_controller/lib/screens/the_big_place_screen.dart
+++ siot_room_controller/lib/screens/the_big_place_screen.dart
@@ -8,10 +8,13 @@ class TheBigPlaceScreen extends StatefulWidget {
 }
 
 class _TheBigPlaceScreenState extends State<TheBigPlaceScreen> {
-  int selected = 0;
+  int selectedLayout = 0;
+  bool projectorOne = false;
+  bool projectorTwo = true;
+  bool projectorThree = false;
 
   Widget middleContent() {
-    if (selected == 0) {
+    if (selectedLayout == 0) {
       return SizedBox(
         height: MediaQuery.of(context).size.height * 0.60,
       );
@@ -60,7 +63,57 @@ class _TheBigPlaceScreenState extends State<TheBigPlaceScreen> {
     }
   }
 
-  bool isSwitched = false;
+  Widget projectorWidget(name, state) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.3333,
+      color: state == true ? Color(0xB3007C89) : Color(0xB3000000),
+      child: Center(
+        child: Text(
+          "$name",
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w900,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget layoutWidget(selectedLayoutNumber, imgPathStr) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.3333,
+      // color: Colors.yellow,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage("$imgPathStr"),
+        ),
+      ),
+      child: Align(
+        alignment: FractionalOffset.bottomCenter,
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.06,
+          width: double.infinity,
+          color: selectedLayout == selectedLayoutNumber
+              ? Color(0xB3007C89)
+              : Color(
+                  (0xB3000000),
+                ),
+          child: Center(
+            child: Text(
+              "Layout $selectedLayoutNumber",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,19 +131,57 @@ class _TheBigPlaceScreenState extends State<TheBigPlaceScreen> {
                 showModalBottomSheet<void>(
                     context: context,
                     builder: (BuildContext context) {
-                      return new Container(
-                        height: MediaQuery.of(context).size.height * 0.40,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                Text("Test"),
-                                Switch(value: false, onChanged: (value) {})
-                              ],
-                            )
-                          ],
-                        ),
-                      );
+                      return StatefulBuilder(builder: (BuildContext context,
+                          StateSetter setState /*You can rename this!*/) {
+                        return new Container(
+                          height: MediaQuery.of(context).size.height * 0.40,
+                          width: double.infinity,
+                          // color: Colors.red,
+                          child: Row(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (projectorOne == true) {
+                                      projectorOne = false;
+                                    } else {
+                                      projectorOne = true;
+                                    }
+                                  });
+                                },
+                                child: projectorWidget(
+                                    "Projector 1", projectorOne),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (projectorTwo == true) {
+                                      projectorTwo = false;
+                                    } else {
+                                      projectorTwo = true;
+                                    }
+                                  });
+                                },
+                                child: projectorWidget(
+                                    "Projector 2", projectorTwo),
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    if (projectorThree == true) {
+                                      projectorThree = false;
+                                    } else {
+                                      projectorThree = true;
+                                    }
+                                  });
+                                },
+                                child: projectorWidget(
+                                    "Projector 3", projectorThree),
+                              ),
+                            ],
+                          ),
+                        );
+                      });
                     });
               }),
         ],
@@ -109,7 +200,7 @@ class _TheBigPlaceScreenState extends State<TheBigPlaceScreen> {
         child: Column(
           children: [
             Container(
-              color: selected == 0
+              color: selectedLayout == 0
                   ? Color(0xB3000000)
                   : Color(
                       (0xB3007C89),
@@ -117,7 +208,9 @@ class _TheBigPlaceScreenState extends State<TheBigPlaceScreen> {
               height: MediaQuery.of(context).size.height * 0.15,
               child: Center(
                 child: Text(
-                  selected == 0 ? "Select a Layout" : "Drag Source to Screen",
+                  selectedLayout == 0
+                      ? "Select a Layout"
+                      : "Drag Source to Screen",
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 42,
@@ -137,115 +230,21 @@ class _TheBigPlaceScreenState extends State<TheBigPlaceScreen> {
                   children: [
                     GestureDetector(
                       onTap: () => setState(() {
-                        selected = 1;
+                        selectedLayout = 1;
                       }),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.3333,
-                        // color: Colors.yellow,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image:
-                                AssetImage("assets/images/the_big_place.png"),
-                          ),
-                        ),
-                        child: Align(
-                          alignment: FractionalOffset.bottomCenter,
-                          child: Container(
-                            height: MediaQuery.of(context).size.height * 0.06,
-                            width: double.infinity,
-                            color: selected == 1
-                                ? Color(0xB3007C89)
-                                : Color(
-                                    (0xB3000000),
-                                  ),
-                            child: Center(
-                              child: Text(
-                                "Layout 1",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 36,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                      child: layoutWidget(1, "assets/images/the_big_place.png"),
                     ),
                     GestureDetector(
                       onTap: () => setState(() {
-                        selected = 2;
+                        selectedLayout = 2;
                       }),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.3333,
-                        // color: Colors.yellow,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: AssetImage("assets/images/open_place.png"),
-                          ),
-                        ),
-                        child: Align(
-                          alignment: FractionalOffset.bottomCenter,
-                          child: Container(
-                            height: MediaQuery.of(context).size.height * 0.06,
-                            width: double.infinity,
-                            color: selected == 2
-                                ? Color(0xB3007C89)
-                                : Color(
-                                    (0xB3000000),
-                                  ),
-                            child: Center(
-                              child: Text(
-                                "Layout 2",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 36,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                      child: layoutWidget(2, "assets/images/open_place.png"),
                     ),
                     GestureDetector(
                       onTap: () => setState(() {
-                        selected = 3;
+                        selectedLayout = 3;
                       }),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.3333,
-                        // color: Colors.yellow,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: AssetImage("assets/images/townhall.png"),
-                          ),
-                        ),
-                        child: Align(
-                          alignment: FractionalOffset.bottomCenter,
-                          child: Container(
-                            height: MediaQuery.of(context).size.height * 0.06,
-                            width: double.infinity,
-                            color: selected == 3
-                                ? Color(0xB3007C89)
-                                : Color(
-                                    (0xB3000000),
-                                  ),
-                            child: Center(
-                              child: Text(
-                                "Layout 3",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 36,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
+                      child: layoutWidget(3, "assets/images/townhall.png"),
                     ),
                   ],
                 ),
